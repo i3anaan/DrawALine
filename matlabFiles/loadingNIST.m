4d74a7a6a1022491dba9e758d51e9fbca15e1bcc
Anna <annakosiorek4@gmail.com>
2017-11-23 15:37:24 +0100

    matlab funtion

---
+++ matlabFiles/loadingNIST.m
@@ -0,0 +1,16 @@
+a=prnist([5,9],[1:10]);
+%nisttrain_cell, cell datafile with 40 objects in 2 crisp classes: [20  20]
+%show(a)
+% add rows and columns to create square figure (aspect ratio 1)
+b = im_box(a,[],1); %figure; show(b)
+% resample by 16 x 16 pixels
+c = im_resize(b,[16,16]); %figure; show(c)
+
+mat = c*data2im()
+
+
+% compute means
+%d = im_mean(c);
+% convert to PRTools dataset and display
+%x = prdataset(d,[],'featlab',char('mean-x','mean-y'));
+%scatterd(x,'legend');
