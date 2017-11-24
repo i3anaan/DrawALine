294275097a25ba4e9472c6a56647312758e76c3e
rolf <rvers@protonmail.com>
2017-11-24 11:36:27 +0100

    logistic regression 98.8% accuracy on the training set

--- test.py
+++ test.py
@@ -1,13 +1,38 @@
 import scipy.io
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.misc import toimage
+import matplotlib
+#import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
-labels = scipy.io.loadmat('./matlabFiles/label.mat')
-imgs = scipy.io.loadmat('./matlabFiles/data.mat')
+# this may be redundant
+matplotlib.rcParams['backend'] = "Qt4Agg"
+
+# load the data
+X = scipy.io.loadmat('./matlabFiles/data.mat')['data']
+y = scipy.io.loadmat('./matlabFiles/label.mat')['labels'].ravel() - 1
+
+# print the shapes
+print("size of X: " + str(X.shape))
+print("size of y: " + str(y.shape))
 
 #image processing
-img = imgs['data'][0,:]
-img = np.reshape(img,(16,16)).T
-plt.imshow(img, cmap='gray')
-plt.show()
+#img = X[0, :]
+#img = np.reshape(img, (16, 16)).T
+#print(img)
+#plt.imshow(img, cmap='gray')
+#plt.savefig('test.png')
+
+# build the model
+logistic = LogisticRegression()
+logistic.fit(X, y)
+
+# show model predictions on some instances
+for i in range(0, 1999, 200):
+    img = X[i]
+    print(img.reshape((16, 16)).T)
+    prediction = logistic.predict(np.array([img]))
+    print("Predicted value: " + str(prediction[0]))
+    print("True value:      " + str(y[i]))
+
+# overall accuracy of the model
+print("Overall accuracy: " + str(logistic.score(X, y)))
