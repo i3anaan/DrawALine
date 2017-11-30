dca234c3b539e1e9d81fe29e6b6a1cfe7778d0ba
rolf <rvers@protonmail.com>
2017-11-30 14:49:49 +0100

    moved the visualisation into a separate function

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
+import sys
 import scipy.io
 import numpy as np
 import matplotlib
-import sys
 #import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 
@@ -12,13 +12,14 @@ from sklearn.neural_network import MLPClassifier
 matplotlib.rcParams['backend'] = "Qt4Agg"
 
 # load the data
-X = scipy.io.loadmat('./matlabFiles/data.mat')['data']
-y = scipy.io.loadmat('./matlabFiles/label.mat')['labels'].ravel() - 1
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X = scipy.io.loadmat('./matlabFiles/data28.mat')['matImages'][0]
+X = np.array([x.reshape((784,)) for x in X])
+y = scipy.io.loadmat('./matlabFiles/labels28.mat')['matLabels'].ravel() - 1
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
 
 # print the shapes
-print("size of X: " + str(X.shape))
-print("size of y: " + str(y.shape))
+print("Training set size: " + str(X_train.shape))
+print("Test set size:     " + str(X_test.shape))
 
 #image processing
 #img = X[0, :]
@@ -27,34 +28,38 @@ print("size of y: " + str(y.shape))
 #plt.imshow(img, cmap='gray')
 #plt.savefig('test.png')
 
-def visualizeImage( img ):
-   image = np.array_str(img)
-   image = image.replace('0', ' ').replace('1', '#')
-   return image
-
 # build the model
-logistic = LogisticRegression(max_iter=1000, C=0.1)
+logistic = LogisticRegression(max_iter=1000, C=0.01)
 logistic.fit(X_train, y_train)
-svm_model = SVC()
-svm_model.fit(X_train, y_train)
-clf = MLPClassifier(solver='lbfgs', alpha=3, hidden_layer_sizes=(25, 10), random_state=1)
-clf.fit(X_train, y_train)
-
+#svm_model = SVC(C=3) #10
+#svm_model.fit(X_train, y_train)
+#alpha_test = 0.03
+#print("alpha: " + str(alpha_test))
+#clf = MLPClassifier(solver='adam', alpha=alpha_test, hidden_layer_sizes=(800, 10), random_state=1, max_iter=10000)
+#clf.fit(X_train, y_train)
 
 # overall accuracy of the model
 print("Accuracy Logistic Regression: " + str(logistic.score(X_train, y_train)) + " - " + str(logistic.score(X_test, y_test)))
-print("Accuracy Support Vector Machine: " + str(svm_model.score(X_train, y_train)) + " - " + str(svm_model.score(X_test, y_test)))
-print("Accuracy Neural Network: " + str(clf.score(X_train, y_train)) + " - " + str(clf.score(X_test, y_test)))
+#print("Accuracy Support Vector Machine: " + str(svm_model.score(X_train, y_train)) + " - " + str(svm_model.score(X_test, y_test)))
+#print("Accuracy Neural Network: " + str(clf.score(X_train, y_train)) + " - " + str(clf.score(X_cv, y_cv)))
 
-if (len(sys.argv) <= 1 or sys.argv[1] != "--no-examples"):
-    print("some examples from the NN:")
-    # show model predictions on some instances
-    for i in range(0, 399):
-        img = X_test[i]
-        print(visualizeImage(img.reshape((16, 16)).T))
-        prediction = clf.predict(np.array([img]))
-        print("Predicted value: " + str(prediction[0]))
-        print("True value:      " + str(y_test[i]))
+def visualize_img (img):
+    image = np.array_str(img)
+    image = image.replace('0', ' ').replace('1', '#')
+    return image
+
+# show model predictions on some instances
+def print_examples(model, test_set, test_set_answers):
+    print("some examples: ")
+    for i in range(0, len(test_set)):
         userin = input("Continue? (Y/n):")
         if userin == "n":
             break
+        img = test_set[i]
+        print(visualize_img(img.reshape((28, 28))))
+        prediction = model.predict(np.array([img]))
+        print("Predicted value: " + str(prediction[0]))
+        print("True value:      " + str(test_set_answers[i]))
+
+if (len(sys.argv) <= 1 or sys.argv[1] != "--no-examples"):
+    print_examples(logistic, X_test, y_test)
