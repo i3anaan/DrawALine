270c84df27d42f8f5574f1ab4c7878cb40f29aad
Anna <annakosiorek4@gmail.com>
2018-01-11 16:56:06 +0100

    pca fixes

--- src/main.py
+++ src/main.py
@@ -20,14 +20,15 @@ def main():
     y_full = scipy.io.loadmat(
         '../matlabFiles/labels28.mat')['labels28'].ravel() - 1
 
-    # Split the data set
-    if (option_set("--small")):
-        print("Cherry picking data set...")
-        X_full, y_full = cherry_pick_data_set(10, X_full, y_full)
-
     print("Splitting the data set...")
     X_train, X_test, y_train, y_test = train_test_split(
         X_full, y_full, test_size=0.1, random_state=1)
+
+    # Split the data set
+    if (option_set("--small")):
+        print("Cherry picking data set...")
+        X_train, y_train = cherry_pick_data_set(10, X_full, y_full)
+
 
     # Optionally extend the data set by using distortions
     if (option_set("--distort")):
@@ -38,11 +39,10 @@ def main():
     print("Training set size: " + str(X_train.shape))
     print("Test set size:     " + str(X_test.shape))
 
-    if (option_set("knn")):
-        cls_knn.knn_svd_pca(X_full, y_full, output_result)
     if (option_set("knn-pca")):
-        cls_knn.testAccuracy(X_train, y_train, X_test, y_test, 0,
-                             output_result)
+        cls_knn.knn_pca(X_train, y_train, X_test, y_test, output_result)
+    if (option_set("knn")):
+        cls_knn.testAccuracy(X_train, y_train, X_test, y_test, 0, output_result)
     if (option_set("svc")):
         cls_svc.testAccuracy(X_train, y_train, X_test, y_test, output_result)
     if (option_set("mlp")):
@@ -58,11 +58,10 @@ def cherry_pick_data_set(amount, X_full, y_full):
     step = round(length / 10)
     X_small = []
     y_small = []
-    #X_small.append(X_full[0:length:step])
     for i in range(10):
-        X_small.extend(X_full[step * i:(step * i + amount)])
+        X_small.extend(X_full[step * i:((step * i) + amount)])
         y_small.extend(y_full[step * i:(step * i + amount)])
-    return X_small, y_small
+    return np.array(X_small), np.array(y_small)
 
 
 def output_result(model, X_train, y_train, X_test, y_test):
